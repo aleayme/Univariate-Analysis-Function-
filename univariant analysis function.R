a913f8bc38c44fc624df6ba594a7f2976d96b09a
aleayme <125659366+aleayme@users.noreply.github.com>
2025-12-09 10:16:11 +0100

    Enhance univariant_analysis with id_check parameter
    Added id_check parameter to univariant_analysis function for optional identifier analysis skipping.

--- univariant analysis function.R
+++ univariant analysis function.R
@@ -1,10 +1,10 @@
-
 univariant_analysis <- function(data, 
-                                variable = NULL, 
+                                variable = NULL,
+                                id_check = FALSE,
                                 plot_type = "ggplot", 
                                 plot_num = "hist",     
                                 plot_cat = "bar",      
-                                max_cat_plot = 50, # max unique values to plot categorical variables
+                                max_cat_plot = 50, 
                                 num_cat_threshold = 15) { 
   
   # If variable not specified, analyze all variables from dataset 
@@ -33,23 +33,23 @@ univariant_analysis <- function(data,
     
     # classification of the variable/column 
     is_numeric <- is.numeric(original_col)
+    # Logic: It is categorical if it's NOT numeric, OR if it IS numeric but has few unique values
     is_categorical <- !is_numeric || (is_numeric && n_unique <= num_cat_threshold)
-    is_wholenumber <-  !is_numeric || all(original_col %% 1 == 0, na.rm = TRUE)
     
-    # Identifier check
-    if (n_unique == n_rows && n_rows > 1 && is_wholenumber) {
-      cat("   Type: Identifier (ID)\n")
-      cat("   (Summary and plot ommited)\n\n") # no sense to summarize or plot IDs
-      
-      # Categorical variable check
-    } else if (is_categorical) {
-      
+    # ID check, false by default; TRUE when trying to check equal number of samples and unique values
+    if (!id_check) {
+      if (grepl("id$", var_lower) || grepl("^id", var_lower) || grepl("_id_", var_lower) || grepl("\\.id", var_lower)) {
+        cat("   Type: Identifier (ID) - Skipping analysis as per id_check = FALSE\n\n")
+        next # Skip to the next iteration of the loop
+      }
+    }
+    
+    # if for independet analysis on variable from id check 
+    if (is_categorical) {
       
       if (is_numeric) {
         
         # Detection of gender variable by name and values
-        # Search 'sex', 'gender' or 'genero' (for spanish datasets) in the variable name
-        # and check if values are coded as 1 and 2
         if ((grepl("gender", var_lower) || grepl("genero", var_lower) || grepl("sex", var_lower)) && 
             all(ordered_uniq_val %in% c(1, 2))) {
           current_col <- factor(current_col, levels = c(1, 2), labels = c("Female", "Male"))
@@ -78,7 +78,6 @@ univariant_analysis <- function(data,
       # Summary of categorical variable
       cat("   Unique values:", n_unique, "\n")
       counting <- table(current_col)
-      proportions <- prop.table(counting)
       
       print(counting)
       cat("\n")
@@ -102,7 +101,7 @@ univariant_analysis <- function(data,
             barplot(counting, main = paste("Barplot of", var), col = rainbow(length(counting)))
           }
           
-        # pie chart
+          # pie chart
         } else if (plot_cat == "pie") {
           cat("   Pie chart...\n")
           df_pie <- as.data.frame(counting)
@@ -122,8 +121,8 @@ univariant_analysis <- function(data,
         }
       }
       
-      # Numerical category 
     } else {
+      # Numerical category 
       cat("   Type: Numerical Continuous\n")
       
       # Basic summary statistics
@@ -137,7 +136,7 @@ univariant_analysis <- function(data,
         if (plot_type == "ggplot") {
           p <- ggplot(data, aes_string(x = var)) +
             geom_histogram(aes(fill = ..x..), bins = 30, color = "black", alpha = 0.8) +
-            scale_fill_gradient(low = "mediumpurple", high = "salmon") + # Color gradient based on value
+            scale_fill_gradient(low = "mediumpurple", high = "salmon") + 
             labs(title = paste("Histogram of", var), x = var, y = "Frequency") +
             theme_minimal() +
             theme(legend.position = "none")
@@ -153,9 +152,8 @@ univariant_analysis <- function(data,
         cat("   Boxplot...\n")
         # with ggplot
         if (plot_type == "ggplot") {
-          # Para boxplot único, coloreamos el relleno para que no sea plano
           p <- ggplot(data, aes_string(x = "factor(1)", y = var)) + 
-            geom_boxplot(fill = "mediumpurple", alpha = 0.7, outlier.colour = "grey") +  # outlier in gray with transparency for frequency 
+            geom_boxplot(fill = "mediumpurple", alpha = 0.7, outlier.colour = "grey") + # grey transparency outliers to check frequency
             labs(title = paste("Boxplot of", var), y = var, x = "") +
             theme_minimal() +
             theme(axis.text.x = element_blank()) 
